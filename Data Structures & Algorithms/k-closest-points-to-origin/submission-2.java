aa5f110d92fd3324b0b6a0678f8681f34716aed1
Vaibhav Kumar <44197429+Vai-bhav@users.noreply.github.com>
2026-07-24 20:03:40 +0530

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.java
@@ -0,0 +1,36 @@
+class Tuple {
+    int x;
+    int y;
+    int dist;
+
+    public Tuple (int x, int y, int dist) {
+        this.x = x;
+        this.y = y;
+        this.dist = dist;
+    }
+}
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        if (points.length <= k) return points;
+
+        PriorityQueue<Tuple> pq = new PriorityQueue<>((a, b) -> (b.dist - a.dist));
+
+        for (int i=0;i<points.length;i++) {
+            int x = points[i][0] * points[i][0];
+            int y = points[i][1] * points[i][1];
+            int euclidDistance = x+ y;
+
+            pq.offer(new Tuple(points[i][0], points[i][1], euclidDistance));
+            if (pq.size() > k) pq.poll();
+        }
+
+        int[][] res = new int[k][2];
+        for (int i=0;i<k;i++) {
+            Tuple peek = pq.poll();
+            res[i] = new int[]{peek.x, peek.y};
+        }
+
+        return res;
+    }
+}
