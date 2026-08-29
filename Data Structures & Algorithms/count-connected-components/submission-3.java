b71d87e0f3b5787ff70404947de87ab446f79f78
Vaibhav Kumar <44197429+Vai-bhav@users.noreply.github.com>
2026-08-29 13:29:50 +0530

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.java
@@ -0,0 +1,52 @@
+class DisjointSet {
+    int[] size;
+    int[] parent;
+    private int count;
+    public DisjointSet(int n) {
+        size = new int[n];
+        parent = new int[n];
+        count = n;
+        for (int i=0;i<n;i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public void unionBySize(int u, int v) {
+        int uParent = parent(u);
+        int vParent = parent(v);
+
+        if(uParent == vParent) return;
+        else if (size[uParent] > size[vParent]) {
+            parent[vParent] = uParent;
+            size[uParent] += size[vParent];
+            count--;
+        } else {
+            parent[uParent] = vParent;
+            size[vParent] += size[uParent];
+            count--;
+        }
+    }
+
+    public int parent(int u) {
+        if (parent[u] == u) return u;
+
+        return parent[u] = parent(parent[u]);
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        DisjointSet dsu = new DisjointSet(n);
+
+        for (int[] edge: edges) {
+            dsu.unionBySize(edge[0], edge[1]);
+        }
+
+        return dsu.getCount();
+    }
+}
