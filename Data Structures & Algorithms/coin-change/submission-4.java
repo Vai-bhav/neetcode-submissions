74ff03088099c49458b7f0cc98e7486e3506a1bb
Vaibhav Kumar <44197429+Vai-bhav@users.noreply.github.com>
2026-09-10 11:22:13 +0530

    Add: coin-change - submission-4

---
+++ Data Structures & Algorithms/coin-change/submission-4.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[] prev = new int[amount+1];
+        int[] curr = new int[amount+1];
+
+        for (int i=0;i<=amount;i++) {
+            if (i % coins[0] == 0) prev[i] = i / coins[0];
+            else prev[i] = (int) 1e9;
+        }
+
+        curr = prev;
+
+        for (int i=1;i<n;i++) {
+            for (int j=0;j<=amount;j++) {
+                int notTake = prev[j];
+                int take = (int) 1e9;
+                if (j >= coins[i]) take = 1 + curr[j-coins[i]];
+
+                curr[j] = Math.min(take, notTake);
+            }
+
+            prev = curr;
+        }
+
+        return (curr[amount] >= (int) 1e9) ? -1 : curr[amount];
+    }
+}
