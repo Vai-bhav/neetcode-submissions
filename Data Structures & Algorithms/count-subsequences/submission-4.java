e7e91de4252dd4559abdd117517900f031fb1f25
Vaibhav Kumar <44197429+Vai-bhav@users.noreply.github.com>
2026-09-14 09:54:16 +0530

    Add: count-subsequences - submission-4

---
+++ Data Structures & Algorithms/count-subsequences/submission-4.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+        int[] prev = new int[m+1];
+
+        prev[0] = 1;
+        for (int j=1;j<=m;j++) prev[j] = 0;
+
+        for (int i=1;i<=n;i++) {
+            int[] curr = new int[m+1];
+            curr[0] = 1;
+            for (int j=1;j<=m;j++) {
+                if (s.charAt(i-1) == t.charAt(j-1)) curr[j] = prev[j] + prev[j-1];
+                else curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+
+        return prev[m];
+    }
+}
