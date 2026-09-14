beb3aba53e7ff0499b86b4e934b64125a06444a3
Vaibhav Kumar <44197429+Vai-bhav@users.noreply.github.com>
2026-09-14 10:04:00 +0530

    Add: edit-distance - submission-3

---
+++ Data Structures & Algorithms/edit-distance/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n+1][m+1];
+        for (int i=0;i<=n;i++) Arrays.fill(dp[i], -1);
+
+        return minOperations(word1, word2, n, m, dp);
+    }
+
+    private int minOperations(String word1, String word2, int i, int j, int[][] dp) {
+        if (i==0 && j==0) return 0;
+        if (i>0 && j==0) return i;
+        if (i==0 && j>0) return j;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (word1.charAt(i-1) == word2.charAt(j-1)) return dp[i][j] = minOperations(word1, word2, i-1, j-1, dp);
+        return dp[i][j] = 1 + Math.min(minOperations(word1, word2, i-1, j-1, dp), Math.min(minOperations(word1, word2, i, j-1, dp), minOperations(word1, word2, i-1, j, dp)));
+    }
+}
